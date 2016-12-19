27c3e042293d054b3b9aee272cef93165cb592a0
Josh Adams <josh.rubyist@gmail.com>
2016-12-18 21:41:25 -0600

    Update with latest code.

--- src/Main.elm
+++
@@ -1,9 +0,0 @@
-module Main exposing (..)
-
-import App exposing (..)
-import Html exposing (program)
-
-
-main : Program Never Model Msg
-main =
-    program { view = view, init = init, update = update, subscriptions = subscriptions }
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-elm package install -y && \
-  npm install && \
-  bower install && \
-  npm run start
